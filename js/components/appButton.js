52c60ed4aa3bb32a7a0810dfe5a2e9dc7f2f0a03
ywonson <ywson51@gmail.com>
2023-06-22 17:37:48 +0900

    make into component file

---
+++ js/components/appButton.js
@@ -0,0 +1,10 @@
+export default {
+template: `<button class="bg-green-200 hover:bg-green-400 border rounded px-4 py-2 disabled:cursor-not-allowed" :disabled="processing">
+                        <slot />
+                        </button>`,
+data() {
+                        return{
+                            processing: false,
+                        };
+                    }
+                }
