80537b4389582b5d97cf372586e4a2d8158ccdd4
ywonson <ywson51@gmail.com>
2023-06-22 18:30:34 +0900

    class binding with props

--- js/components/appButton.js
+++ js/components/appButton.js
@@ -1,7 +1,12 @@
 export default {
-template: `<button class="bg-green-200 hover:bg-green-400 border rounded px-4 py-2 disabled:cursor-not-allowed" :disabled="processing">
-                        <slot />
-                        </button>`,
+template: `<button 
+            :class="{
+                'px-4 py-2 disabled:cursor-not-allowed' : true,
+                'bg-green-200 hover:bg-green-400 border rounded' : type === 'normal',
+            }"
+            :disabled="processing">
+                <slot />
+            </button>`,
 data() {
                         return{
                             processing: false,
